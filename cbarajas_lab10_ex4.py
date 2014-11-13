da417c296a4596e0f830ac1a81dd697604f322b6
Carlos Barajas <12barajasc@gmail.com>
2014-11-13 17:24:59 -0500

    Rough outline

--- cbarajas_lab10_ex4.py
+++ cbarajas_lab10_ex4.py
@@ -1,1 +1,18 @@
 import tkinter
+window = tkinter.Tk()
+frame = tkinter.Frame(window)
+frame.pack()
+dna = tkinter.StringVar()
+(A, T, C, G) = (tkinter.IntVar().set(0), tkinter.IntVar().set(0),
+				tkinter.IntVar().set(0), tkinter.IntVar().set(0))
+entry = tkinter.Entry(frame, textvariable=dna)
+entry.pack()
+button = tkinter.Button(frame, text='Count', command=lambda: tally(dna))
+button.pack()
+label = tkinter.Label(frame, textvariable=A)
+label.pack()
+def tally(dnastring):
+	A.set(dnastring.get().upper().count('A'))
+
+
+window.mainloop()
