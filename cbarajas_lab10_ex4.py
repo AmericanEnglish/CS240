14fc2b14e168420628d9f4887d439a6ef39ffd85
Carlos Barajas <12barajasc@gmail.com>
2014-11-13 17:55:33 -0500

    Shortening

--- cbarajas_lab10_ex4.py
+++ cbarajas_lab10_ex4.py
@@ -1,22 +1,27 @@
 import tkinter
 window = tkinter.Tk()
+
 frame = tkinter.Frame(window)
 frame.pack()
+
 dna = tkinter.StringVar()
-A, C, G, T = tkinter.IntVar(), tkinter.IntVar(), tkinter.IntVar(), tkinter.IntVar()
+
 entry = tkinter.Entry(frame, textvariable=dna)
 entry.pack()
+
 button = tkinter.Button(frame, text='Count', command=lambda: tally(dna))
 button.pack()
-label = tkinter.Label(frame, text='Num As: 0 Num Ts: 0 Num Cs: 0 Num Gs: 0')
-label.pack()
-def tally(dnastring):
-	A.set(dnastring.get().upper().count('A'))
-	C.set(dnastring.get().upper().count('C'))
-	G.set(dnastring.get().upper().count('G'))
-	T.set(dnastring.get().upper().count('T'))
-	label.config(text='Num As: {} Num Ts: {} Num Cs: {} Num Gs: {}'.format(
-				A.get(), T.get(), C.get(), G.get()))
 
+label = tkinter.Label(frame, text=output)
+label.pack()
+
+output = 'Num As: {} Num Ts: {} Num Cs: {} Num Gs: {}'.format(
+				A, T, C, G))
+def tally(dnastring):
+	A = dnastring.get().upper().count('A')
+	C = dnastring.get().upper().count('C')
+	G = dnastring.get().upper().count('G')
+	T = dnastring.get().upper().count('T')
+	label.config(text=outfig)
 
 window.mainloop()
