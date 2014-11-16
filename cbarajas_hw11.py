878cd309c5a581bad7539d35953c1abe9eb262f0
Carlos Barajas <12barajasc@gmail.com>
2014-11-15 19:02:27 -0500

    Added FieldOut class

--- cbarajas_hw11.py
+++ cbarajas_hw11.py
@@ -12,7 +12,7 @@ class Field:
         self.label.pack(side='left')
         self.file = tkinter.StringVar()
         self.entry = tkinter.Entry(self.frame, textvariable=self.file)
-        self.entry.pack(side='left')
+        self.entry.pack(side='center')
         self.button = tkinter.Button(self.frame, text=buttonlabel, command=lambda: None)
         self.button.pack(side='right')
 
@@ -23,12 +23,14 @@ class FieldIn(Field):
     def __init__(self, parent, entrylabel, buttonlabel):
         super().__init__(parent, entrylabel, buttonlabel)
         self.button.config(command=lambda: self.filedo(
-                            dialog.askopenfilename(title='File Input')))
+                            dialog.askopenfilename(title='File In')))
         self.entry.config(textvariable=self.file)
 
-class FileOut(Field):
+class FieldOut(Field):
     def __init__(self, parent, entrylabel, buttonlabel):
         super().__init__(parent, entrylabel, buttonlabel)
+        self.button.config(command=lambda: self.filedo(
+                            dialog.asksaveasfilename(title='File Out')))
 
 
 if __name__ == '__main__':
