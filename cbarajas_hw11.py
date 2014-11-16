f53979ec5d2cfadc8f33cf04908eaabd337f8c9c
Carlos Barajas <12barajasc@gmail.com>
2014-11-16 00:24:46 -0500

    Fixed grid issue

--- cbarajas_hw11.py
+++ cbarajas_hw11.py
@@ -4,37 +4,39 @@ import tkinter.filedialog as dialog
 
 class Field:
     ''' generates a input field '''
-    def __init__(self, parent, entrylabel, buttonlabel):
-        self.window = parent
-        self.frame = tkinter.Frame(window)
-        self.frame.pack()
+    def __init__(self, frameZ, entrylabel, buttonlabel, n):
+        self.frame = frameZ
+        self.frame.grid(row=n)
         self.label = tkinter.Label(self.frame, text=entrylabel)
-        self.label.pack(side='left')
+        self.label.grid(row=n, column=0, sticky='W', padx=20)
         self.file = tkinter.StringVar()
         self.entry = tkinter.Entry(self.frame, textvariable=self.file)
-        self.entry.pack(side='left')
+        self.entry.grid(row=n, column=1)
         self.button = tkinter.Button(self.frame, text=buttonlabel, command=lambda: None)
-        self.button.pack(side='right')
+        self.button.grid(row=n, column=2)
 
     def filedo(self, filename):
         self.file.set(filename)
 
 class FieldIn(Field):
-    def __init__(self, parent, entrylabel, buttonlabel):
-        super().__init__(parent, entrylabel, buttonlabel)
+    def __init__(self, frameZ, entrylabel, buttonlabel, n):
+        super().__init__(parent, framez, entrylabel, buttonlabel, n)
         self.button.config(command=lambda: self.filedo(
                             dialog.askopenfilename(title='File In')))
         self.entry.config(textvariable=self.file)
 
 class FieldOut(Field):
-    def __init__(self, parent, entrylabel, buttonlabel):
-        super().__init__(parent, entrylabel, buttonlabel)
+    def __init__(self, frameZ, entrylabel, buttonlabel, n):
+        super().__init__(parent, frameZ, entrylabel, buttonlabel, n)
         self.button.config(command=lambda: self.filedo(
                             dialog.asksaveasfilename(title='File Out')))
 
-
+class ProcessButton:
+    def __init__(self, frameZ):
+        pass
 if __name__ == '__main__':
     window = tkinter.Tk()
-    field1 = FieldIn(window, 'Input', 'Browse...')
-    field2 = FieldOut(window, 'Output', 'Browse...')
+    frame = tkinter.Frame()
+    field1 = FieldIn(window, frame, 'Input File:', 'Browse...', 0)
+    field2 = FieldOut(window, frame, 'Output File:', 'Browse...', 1)
     window.mainloop()
